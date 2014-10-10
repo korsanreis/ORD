2bf3d39e35f106ea279dbae9f15cf89b587a144d
Jean-Roger <jeanroger.orsini@gmail.com>
2014-10-10 12:35:22 -0700

    Adding new files index.php qnd header.html + .gitignore

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="style.css">
+	<title>OscaroParts.com</title>
+</head>
+<body>
+	<div class="offCanvasBackground"></div>
+	<div class="SubOffCanvasBackground"></div>
+	<?php require 'header.html'; ?>
+
+</body>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="demo.js"></script>
+</html>
