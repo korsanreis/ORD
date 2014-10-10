6f563df20057a22d391e47c6bcb36c7b59921675
Jean-Roger <jeanroger.orsini@gmail.com>
2014-10-10 13:20:03 -0700

    Designed search bar nav in Universale Search

--- index.php
+++ index.php
@@ -10,9 +10,20 @@
 	<div class="SubOffCanvasBackground"></div>
 	<?php require 'header.html'; ?>
 	<form>
-		<input type="text" class="searchBar">		
+		<input type="text" class="searchBar" placeholder="Search by Part Number">		
 		<button>Go</button>
 	</form>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in est tincidunt, volutpat tortor in, volutpat sem. Mauris aliquam lobortis arcu ac viverra. Curabitur feugiat, ante vel volutpat vehicula, turpis ante fermentum justo, et molestie nulla est in tortor. Curabitur id fermentum ipsum. Donec congue sem at lectus porta, vel ornare velit posuere. Ut porta dui at porta finibus. Mauris aliquet lacus dolor, vitae elementum mauris gravida id. Vestibulum viverra fermentum augue, vehicula egestas nibh pellentesque scelerisque. Morbi vehicula efficitur neque at lacinia.
+		<br><br>
+		Vivamus rhoncus cursus tempor. Nunc sollicitudin malesuada ex sit amet porttitor. Integer id interdum nisl. Donec erat est, volutpat venenatis mollis vitae, euismod blandit quam. Mauris fringilla odio a augue hendrerit facilisis. Donec accumsan dui a dui tincidunt consequat. Phasellus at ligula ac orci consectetur aliquam id in ex. Vestibulum euismod lorem felis, ac feugiat mi rhoncus nec.
+		<br><br>
+		Morbi nec metus id augue rutrum aliquam. Donec euismod ligula non erat pulvinar semper vitae in nunc. Vestibulum iaculis, metus a volutpat bibendum, odio tortor ultrices orci, id convallis odio neque vel orci. Quisque pharetra turpis et felis sodales malesuada. Sed libero risus, molestie et purus quis, congue tempus dolor. Duis maximus, arcu sit amet congue pharetra, mi neque vehicula tellus, at consequat urna nunc nec tellus. Vivamus pharetra turpis nec lectus imperdiet interdum. Pellentesque sem nulla, consectetur et fringilla eu, imperdiet in libero. Praesent ac tempus arcu. Ut lacus justo, hendrerit id condimentum ac, interdum molestie tellus. Sed gravida lorem enim, a dignissim ligula scelerisque in. Nullam commodo molestie sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vestibulum faucibus neque, sed varius mi suscipit at.
+		<br><br>
+		Ut elementum diam id arcu sollicitudin maximus. Morbi accumsan risus orci, eget maximus libero placerat eget. Aliquam quam dui, ultricies in consequat non, consectetur ut neque. Sed nec quam quis nulla facilisis pharetra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum eu augue id dolor interdum volutpat sit amet sed elit. Nam faucibus tortor felis, et fringilla nunc sodales at. Etiam commodo, diam id dictum hendrerit, tellus mauris facilisis nibh, ac placerat ante elit at metus.
+		<br><br>
+		Sed ut malesuada ligula. Maecenas ut lacinia dolor. Vestibulum molestie sapien ut enim sagittis eleifend. Fusce vitae nisl vel lacus gravida bibendum in quis erat. Aenean tempor mauris nec neque egestas, id facilisis justo tempus. Pellentesque tempor volutpat consectetur. Fusce non lacus eget est finibus ultrices. Suspendisse et velit ligula. Etiam vitae varius dolor.
+	</p>
 </body>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="demo.js"></script>
