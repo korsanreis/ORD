7472ff8cbb7c3a9fb6d5cd105c56dae4cfbf46b3
Jean-Roger <jeanroger.orsini@gmail.com>
2014-10-15 16:28:23 -0700

    Starting with swiper script for caterories slider launch

--- index.php
+++ index.php
@@ -3,53 +3,43 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="swipe.css">
 	<title>OscaroParts.com</title>
 </head>
 <body>
 	<div class="offCanvasBackground"></div>
 	<div class="SubOffCanvasBackground"></div>
 	<?php require 'header.html'; ?>
-	<form>
-		<input type="text" class="searchBar" placeholder="Search by Part Number">		
-		<button>Go</button>
-	</form>
-	<div class="USTabs">
-		<div class="ymmeVinBtnSection">
-			<button href="#YMME-tab" class="current-tab-selected">VEHICLE</button>
-			<button href="#VIN-tab">VIN NUMBER</button>
-		</div>
-		<div class="tabsSection">
-	        <div id="YMME-tab" class="tab-content">
-				<select>
-					<option value="" disabled selected>Year</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-				</select>
-				<select>
-					<option value="" disabled selected>Make</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-				</select>	
-				<select>
-					<option value="" disabled selected>Model</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-					<option value="">Test</option>
-				</select>	
-				<button>FIND</button>        
-	        </div>
-	       	<div id="VIN-tab" class="tab-content">
-	       		<div class="VIN-tab-wrapper">
-					<input type="text" placeholder="EX : WXYZ12WX123456789">
-					<button>GO</button> 
-	       		</div>
-			</div>
-		</div>
+	<?php require 'search.html'; ?>
+	<div class="categories-container">
+	    <a class="arrows arrow-left" href="#"><</a> 
+	    <a class="arrows arrow-right" href="#">></a>
+	    <div class="swiper-container">
+			<div class="swiper-slide"><img src="img/brakes.png"></div>
+			<div class="swiper-slide"><img src="img/wiper.png"></div>
+			<div class="swiper-slide"><img src="img/air_fuel.png"></div>
+			<div class="swiper-slide"><img src="img/electrical_lighting.png"></div>
+	    </div>
 	</div>
+    <!-- END BODY -->
 </body>
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="swipe.js"></script>
 <script src="demo.js"></script>
+<script>
+	var mySwiper = new Swiper('.swiper-container',{
+		pagination: '.pagination',
+		loop:true,
+		grabCursor: true,
+		paginationClickable: true
+	})
+	$('.arrow-left').on('click', function(e){
+		e.preventDefault()
+		mySwiper.swipePrev()
+	})
+	$('.arrow-right').on('click', function(e){
+		e.preventDefault()
+		mySwiper.swipeNext()
+	})
+</script>
 </html>
