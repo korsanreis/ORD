62ca8f7213649baa45d3acfcb802717b27b228e2
Jean-Roger <jeanroger.orsini@gmail.com>
2014-10-15 18:11:13 -0700

    Following and experiment for swiper slider

--- index.php
+++ index.php
@@ -2,44 +2,51 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="swipe.css">
+	<link rel="stylesheet" href="css/style.css">
+  	<link rel="stylesheet" href="css/idangerous.swiper.css">
 	<title>OscaroParts.com</title>
 </head>
 <body>
 	<div class="offCanvasBackground"></div>
 	<div class="SubOffCanvasBackground"></div>
 	<?php require 'header.html'; ?>
-	<?php require 'search.html'; ?>
-	<div class="categories-container">
-	    <a class="arrows arrow-left" href="#"><</a> 
-	    <a class="arrows arrow-right" href="#">></a>
-	    <div class="swiper-container">
-			<div class="swiper-slide"><img src="img/brakes.png"></div>
-			<div class="swiper-slide"><img src="img/wiper.png"></div>
-			<div class="swiper-slide"><img src="img/air_fuel.png"></div>
-			<div class="swiper-slide"><img src="img/electrical_lighting.png"></div>
-	    </div>
+	<div id="wrapper">
+		<?php require 'search.html'; ?>
+
+		<div class="categories-container">
+		    <a class="arrows arrow-left" href="#"><</a> 
+		    <div class="swiper-container">
+			    <div class="swipe-wrapper">
+					<div class="swiper-slide"><img src="img/brakes.png"><img src="img/wiper.png"></div>
+					<div class="swiper-slide"><img src="img/wiper.png"><img src="img/air_fuel.png"></div>
+					<div class="swiper-slide"><img src="img/air_fuel.png"><img src="img/electrical_lighting.png"></div>
+					<div class="swiper-slide"><img src="img/electrical_lighting.png"><img src="img/brakes.png"></div>
+			    </div>
+		    </div>
+		    <a class="arrows arrow-right" href="#">></a>
+		</div>
 	</div>
+	
     <!-- END BODY -->
 </body>
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="swipe.js"></script>
-<script src="demo.js"></script>
-<script>
-	var mySwiper = new Swiper('.swiper-container',{
-		pagination: '.pagination',
-		loop:true,
-		grabCursor: true,
-		paginationClickable: true
-	})
-	$('.arrow-left').on('click', function(e){
-		e.preventDefault()
-		mySwiper.swipePrev()
-	})
-	$('.arrow-right').on('click', function(e){
-		e.preventDefault()
-		mySwiper.swipeNext()
-	})
-</script>
+	<script src="js/jquery-1.10.1.min.js"></script>
+	<script src="js/idangerous.swiper.min.js"></script>
+	<script>
+		window.onload = function() {
+			var mySwiper = new Swiper('.swiper-container',{
+				mode:'horizontal',
+				loop:true,
+				grabCursor: true
+			})
+			$('.arrow-left').on('click', function(e){
+				e.preventDefault()
+				mySwiper.swipePrev()
+			})
+			$('.arrow-right').on('click', function(e){
+				e.preventDefault()
+				mySwiper.swipeNext()
+			})
+		});
+	</script>
+	<script src="demo.js"></script>
 </html>
