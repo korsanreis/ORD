cb7b2299aa4e016a76234e03aa301688b6051384
Jean-Roger <jeanroger.orsini@gmail.com>
2014-10-23 17:39:45 -0700

    Finalyze search tab menu

--- js/demo.js
+++ js/demo.js
@@ -52,37 +52,60 @@ $('.tabsSection').addClass('closed-tab');
 $('#YMME-tab').addClass('hide-tab-content');
 $('#VIN-tab').addClass('hide-tab-content');
 
-$('.ymmeVinBtnSection button.icon-car').click(function() {
-    $(this).toggleClass('current-tab-selected');
-    $('.ymmeVinBtnSection button.icon-barcode').removeClass('current-tab-selected');
-    if ($(this).hasClass('current-tab-selected')) {
-        $('.tabsSection').addClass('YMMHeight');
-        $('.tabsSection').removeClass('VINHeight');
-        setTimeout(function(){
-            $('#YMME-tab').removeClass('hide-tab-content');
-            $('#VIN-tab').addClass('hide-tab-content');
-        },300);
-    }else{$('#YMME-tab').addClass('hide-tab-content');
-        setTimeout(function() {
-            $('.tabsSection').removeClass('YMMHeight');
-        },300);
+function swTabHt (aTabHt,bTabHt) {
+    $('.tabsSection').addClass(aTabHt);
+    $('.tabsSection').removeClass(bTabHt);
+}
+
+function swTabCont (aTabCont,bTabCont){
+    $(aTabCont).removeClass('hide-tab-content').css('position','relative');
+    $(bTabCont).addClass('hide-tab-content').css('position','absolute');
+}
+
+$('.ymmeVinBtnSection button.icon-car').click(function(){
+    if ($('.ymmeVinBtnSection button.icon-barcode').hasClass('current-tab-selected')){
+        $(this).toggleClass('current-tab-selected');
+        $('.ymmeVinBtnSection button.icon-barcode').removeClass('current-tab-selected');
+        swTabHt('YMMHeight','VINHeight');
+        $('#VIN-tab').addClass('hide-tab-content');
+        setTimeout(function(){$('#YMME-tab').removeClass('hide-tab-content').css('position','relative');$('#VIN-tab').css('position','absolute');},200);
+    }
+    else{
+        $(this).toggleClass('current-tab-selected');
+        $('.ymmeVinBtnSection button.icon-barcode').removeClass('current-tab-selected');
+        if ($(this).hasClass('current-tab-selected')){
+            swTabHt('YMMHeight','VINHeight');
+            setTimeout(function(){swTabCont('#YMME-tab','#VIN-tab');},200);
+        }else{
+            $('#YMME-tab').addClass('hide-tab-content');
+            setTimeout(function() {$('.tabsSection').removeClass('YMMHeight');},200);
+        }
     }
 });
 
 $('.ymmeVinBtnSection button.icon-barcode').click(function() {
-    $(this).toggleClass('current-tab-selected');
-    $('.ymmeVinBtnSection button.icon-car').removeClass('current-tab-selected');
-    if ($(this).hasClass('current-tab-selected')) {
-        $('.tabsSection').addClass('VINHeight');
-        $('.tabsSection').removeClass('YMMHeight');
-        setTimeout(function(){
-            $('#VIN-tab').removeClass('hide-tab-content');
-            $('#YMME-tab').addClass('hide-tab-content');
-        },300);
-    }else{$('#VIN-tab').addClass('hide-tab-content');
-        setTimeout(function() {
-            $('.tabsSection').removeClass('VINHeight');
-        },300);
+    if ($('.ymmeVinBtnSection button.icon-car').hasClass('current-tab-selected')) {
+        $(this).toggleClass('current-tab-selected');
+        $('.ymmeVinBtnSection button.icon-car').removeClass('current-tab-selected');
+        $('#YMME-tab').addClass('hide-tab-content');
+        setTimeout(function(){swTabHt('VINHeight','YMMHeight');$('#VIN-tab').removeClass('hide-tab-content').css('position','relative');$('#YMME-tab').css('position','absolute');},200);
+    }else{
+        $(this).toggleClass('current-tab-selected');
+        $('.ymmeVinBtnSection button.icon-car').removeClass('current-tab-selected');
+        if ($(this).hasClass('current-tab-selected')) {
+            swTabHt('VINHeight','YMMHeight');
+            setTimeout(function(){
+                swTabCont('#VIN-tab','#YMME-tab');
+            },200);
+        }else{
+            $('#VIN-tab').addClass('hide-tab-content');
+            setTimeout(function() {
+                $('.tabsSection').removeClass('VINHeight');
+                setTimeout(function() {
+                    $('#VIN-tab').css('position','absolute');
+                },200);
+            },200);
+        }
     }
 });
 
